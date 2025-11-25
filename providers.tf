bf4840018b1c6100fb822c867f8570f82f3cbb83
drewpypro <83561525+drewpypro@users.noreply.github.com>
2025-11-25 10:13:57 -0700

    test provider changes

--- providers.tf
+++ providers.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+      version = "~> 5"
+    }
   }
   backend "s3" {
     skip_credentials_validation = true
@@ -11,6 +15,10 @@ terraform {
     skip_metadata_api_check     = true
     skip_region_validation      = true
     skip_s3_checksum            = true
+    use_path_style              = true
     region                      = "wnam"
+    access_key                  = var.BUCKET_ACCESS_KEY_ID
+    secret_key                  = var.BUCKET_SECRET_ACCESS_KEY
+    endpoints                   = { s3 = "https://${var.CLOUDFLARE_ACCOUNT_ID}.r2.cloudstorage.com"}
   }
 }
