24e407cc0604a2e2f43a41bc5591606c7debf64b
drewpypro <83561525+drewpypro@users.noreply.github.com>
2025-11-25 22:15:34 -0700

    updated resources

--- main.tf
+++ main.tf
@@ -20,22 +20,22 @@ locals {
   }
 }
 
-resource "aws_security_group" "sgs" {
-  for_each = local.security_groups
+# resource "aws_security_group" "sgs" {
+#   for_each = local.security_groups
 
-  name        = each.value
-  description = "Managed by Terraform"
-  vpc_id      = var.vpc_id
+#   name        = each.value
+#   description = "Managed by Terraform"
+#   vpc_id      = var.vpc_id
 
-  tags = {
-    Name = each.value
-  }
-}
+#   tags = {
+#     Name = each.value
+#   }
+# }
 
-output "security_group_ids" {
-  value = {
-    for name, sg in aws_security_group.sgs :
-    name => sg.id
-  }
-  description = "Map of all security group names to their IDs"
-}
+# output "security_group_ids" {
+#   value = {
+#     for name, sg in aws_security_group.sgs :
+#     name => sg.id
+#   }
+#   description = "Map of all security group names to their IDs"
+# }
